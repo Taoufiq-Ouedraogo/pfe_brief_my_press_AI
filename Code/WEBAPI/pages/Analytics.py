27dff22d92f2e3cf7c258b73a4cf21eaa4648991
Taoufiq Ouedraogo <taoufiq.ouedraogo@outlook.com>
2025-01-26 16:16:56 +0100

    update page analytics

--- Code/WEBAPI/pages/Analytics.py
+++ Code/WEBAPI/pages/Analytics.py
@@ -45,137 +45,149 @@ Chaque graphique vous permet de déceler les tendances et les variations des don
 
 
 
+# Champ d'entrée pour l'ID
+id_value = st.text_input('Entrez l\'ID de votre média:', value="exemple: bmp_media1")
 
 
-df = pd.read_excel('code/WEBAPI/historique_articles.xlsx')
+df = pd.read_excel('Code/WEBAPI/historique_articles.xlsx')
 df['article_length'] = df['article'].apply(len)
 df['extractiveSummary_length'] = df['extractiveSummary'].apply(len)
 df['abstractiveSummary_length'] = df['abstractiveSummary'].apply(len)
 
 
+# Bouton pour appliquer le filtre
+if st.button('Valider'):
 
-kpi_df = pd.DataFrame({
-    'Nombre d\'articles': [len(df)],
-    'Longueur moyenne des articles': [df['article_length'].mean()],
-    'Longueur maximale des articles': [df['article_length'].max()],
-    'Longueur minimale des articles': [df['article_length'].min()],
-    'Longueur moyenne des résumés extractifs': [df['extractiveSummary_length'].mean()],
-    'Longueur maximale des résumés extractifs': [df['extractiveSummary_length'].max()],
-    'Longueur minimale des résumés extractifs': [df['extractiveSummary_length'].min()],
-    'Longueur moyenne des résumés abstractifs': [df['abstractiveSummary_length'].mean()],
-    'Longueur maximale des résumés abstractifs': [df['abstractiveSummary_length'].max()],
-    'Longueur minimale des résumés abstractifs': [df['abstractiveSummary_length'].min()]
-})
+    if id_value not in df['mediaID'].unique():
+        pass
+
+    else:
+        # Filtrer selon l'ID renseigné
+        df = df[df['mediaID'] == id_value]
 
 
-
-# Affichage des graphiques
-fig1 = px.box(df, title="Longueurs des articles et résumés",
-              y=['article_length', 'extractiveSummary_length', 'abstractiveSummary_length'],
-              labels={'value': 'Longueur (caractères)', 'variable': 'Type'},
-              color='variable')
-
-colors = ["purple", "green", "goldenrod"]
-for trace, color in zip(fig1.data, colors):
-    trace.update(line=dict(color=color, width=3))
-
-fig1.update_layout(width=1500, height=700)
-st.plotly_chart(fig1)
+        kpi_df = pd.DataFrame({
+            'Nombre d\'articles': [len(df)],
+            'Longueur moyenne des articles': [df['article_length'].mean()],
+            'Longueur maximale des articles': [df['article_length'].max()],
+            'Longueur minimale des articles': [df['article_length'].min()],
+            'Longueur moyenne des résumés extractifs': [df['extractiveSummary_length'].mean()],
+            'Longueur maximale des résumés extractifs': [df['extractiveSummary_length'].max()],
+            'Longueur minimale des résumés extractifs': [df['extractiveSummary_length'].min()],
+            'Longueur moyenne des résumés abstractifs': [df['abstractiveSummary_length'].mean()],
+            'Longueur maximale des résumés abstractifs': [df['abstractiveSummary_length'].max()],
+            'Longueur minimale des résumés abstractifs': [df['abstractiveSummary_length'].min()]
+        })
 
 
 
-# Distribution des longueurs des articles et résumés
-columns = ['article_length', 'extractiveSummary_length', 'abstractiveSummary_length']
-kde = ff.create_distplot([df[col] for col in columns], 
-        group_labels=columns, show_hist=False, show_rug=False)
+        # Affichage des graphiques
+        fig1 = px.box(df, title="Longueurs des articles et résumés",
+                    y=['article_length', 'extractiveSummary_length', 'abstractiveSummary_length'],
+                    labels={'value': 'Longueur (caractères)', 'variable': 'Type'},
+                    color='variable')
 
-for trace, color in zip(kde.data, colors):
-    trace.update(line=dict(color=color, width=3))
+        colors = ["purple", "green", "goldenrod"]
+        for trace, color in zip(fig1.data, colors):
+            trace.update(line=dict(color=color, width=3))
 
-kde.update_layout(title="Distribution des longueurs des articles et résumés", width=1500, height=700,
-    xaxis_title="Longueur", yaxis_title="Densité", template="plotly_white")
-st.plotly_chart(kde)
-
-
-
-# Longueur moyenne des articles
-st.subheader("Longueur moyenne des articles et des résumés")
-kpi_df_m1 = kpi_df.melt(id_vars=['Nombre d\'articles'], 
-                             value_vars=['Longueur moyenne des articles', 
-                                         'Longueur moyenne des résumés extractifs', 
-                                         'Longueur moyenne des résumés abstractifs'], 
-                             var_name='Statistique', value_name='Valeur')
-
-fig2 = px.bar(kpi_df_m1, y="Statistique", x="Valeur",  color="Statistique", 
-              color_discrete_sequence=["purple", "green", "goldenrod"])
-
-fig2.update_layout(width=1500, height=600)
-st.plotly_chart(fig2)
+        fig1.update_layout(width=1500, height=700)
+        st.plotly_chart(fig1)
 
 
 
+        # Distribution des longueurs des articles et résumés
+        columns = ['article_length', 'extractiveSummary_length', 'abstractiveSummary_length']
+        kde = ff.create_distplot([df[col] for col in columns], 
+                group_labels=columns, show_hist=False, show_rug=False)
+
+        for trace, color in zip(kde.data, colors):
+            trace.update(line=dict(color=color, width=3))
+
+        kde.update_layout(title="Distribution des longueurs des articles et résumés", width=1500, height=700,
+            xaxis_title="Longueur", yaxis_title="Densité", template="plotly_white")
+        st.plotly_chart(kde)
 
 
-# Plot des longueurs minimales
-c = 'Longueur minimale des articles'
-fig11 = px.pie(kpi_df, values=c, color_discrete_sequence=['purple'], hole=0.8, title=c)
-fig11.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig11.update_traces(textinfo='value', textposition='outside', textfont_size=1)
 
+        # Longueur moyenne des articles
+        st.subheader("Longueur moyenne des articles et des résumés")
+        kpi_df_m1 = kpi_df.melt(id_vars=['Nombre d\'articles'], 
+                                    value_vars=['Longueur moyenne des articles', 
+                                                'Longueur moyenne des résumés extractifs', 
+                                                'Longueur moyenne des résumés abstractifs'], 
+                                    var_name='Statistique', value_name='Valeur')
 
-c = 'Longueur minimale des résumés extractifs'
-fig22 = px.pie(kpi_df, values=c, color_discrete_sequence=['green'], hole=0.8, title=c)
-fig22.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig22.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
+        fig2 = px.bar(kpi_df_m1, y="Statistique", x="Valeur",  color="Statistique", 
+                    color_discrete_sequence=["purple", "green", "goldenrod"])
 
-
-c = 'Longueur minimale des résumés abstractifs'
-fig33 = px.pie(kpi_df, values=c, color_discrete_sequence=['goldenrod'], hole=0.8, title=c)
-fig33.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig33.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.plotly_chart(fig11, use_container_width=True)
-with col2:
-    st.plotly_chart(fig22, use_container_width=True)
-with col3:
-    st.plotly_chart(fig33, use_container_width=True)
+        fig2.update_layout(width=1500, height=600)
+        st.plotly_chart(fig2)
 
 
 
 
-# Plot des longueurs maximales
-c = 'Longueur maximale des articles'
-fig111 = px.pie(kpi_df, values=c, color_discrete_sequence=['purple'], hole=0.8, title=c)
-fig111.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig111.update_traces(textinfo='value', textposition='outside', textfont_size=1)
+
+        # Plot des longueurs minimales
+        c = 'Longueur minimale des articles'
+        fig11 = px.pie(kpi_df, values=c, color_discrete_sequence=['purple'], hole=0.8, title=c)
+        fig11.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig11.update_traces(textinfo='value', textposition='outside', textfont_size=1)
 
 
-c = 'Longueur maximale des résumés extractifs'
-fig222 = px.pie(kpi_df, values=c, color_discrete_sequence=['green'], hole=0.8, title=c)
-fig222.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig222.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
+        c = 'Longueur minimale des résumés extractifs'
+        fig22 = px.pie(kpi_df, values=c, color_discrete_sequence=['green'], hole=0.8, title=c)
+        fig22.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig22.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
 
 
-c = 'Longueur maximale des résumés abstractifs'
-fig333 = px.pie(kpi_df, values=c, color_discrete_sequence=['goldenrod'], hole=0.8, title=c)
-fig333.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
-                font_size=20, showarrow=False)], width=500, height=500)
-fig333.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
+        c = 'Longueur minimale des résumés abstractifs'
+        fig33 = px.pie(kpi_df, values=c, color_discrete_sequence=['goldenrod'], hole=0.8, title=c)
+        fig33.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig33.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.plotly_chart(fig111, use_container_width=True)
-with col2:
-    st.plotly_chart(fig222, use_container_width=True)
-with col3:
-    st.plotly_chart(fig333, use_container_width=True)
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.plotly_chart(fig11, use_container_width=True)
+        with col2:
+            st.plotly_chart(fig22, use_container_width=True)
+        with col3:
+            st.plotly_chart(fig33, use_container_width=True)
+
+
+
+
+        # Plot des longueurs maximales
+        c = 'Longueur maximale des articles'
+        fig111 = px.pie(kpi_df, values=c, color_discrete_sequence=['purple'], hole=0.8, title=c)
+        fig111.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig111.update_traces(textinfo='value', textposition='outside', textfont_size=1)
+
+
+        c = 'Longueur maximale des résumés extractifs'
+        fig222 = px.pie(kpi_df, values=c, color_discrete_sequence=['green'], hole=0.8, title=c)
+        fig222.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig222.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
+
+
+        c = 'Longueur maximale des résumés abstractifs'
+        fig333 = px.pie(kpi_df, values=c, color_discrete_sequence=['goldenrod'], hole=0.8, title=c)
+        fig333.update_layout(annotations=[dict(text=str(kpi_df[c][0]), x=0.5, y=0.5, 
+                        font_size=20, showarrow=False)], width=500, height=500)
+        fig333.update_traces(textinfo='value', textposition='outside', textfont_size=1)  
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.plotly_chart(fig111, use_container_width=True)
+        with col2:
+            st.plotly_chart(fig222, use_container_width=True)
+        with col3:
+            st.plotly_chart(fig333, use_container_width=True)
 
 
 
